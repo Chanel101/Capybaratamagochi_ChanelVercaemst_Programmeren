87bce16560f95f2a79216396fa036a98b6715b23
Chanel101 <vercaemstchanel@gmail.com>
2024-05-31 10:07:03 +0200

    magische update

--- js/javascript.js
+++ js/javascript.js
@@ -54,7 +54,7 @@ let state_achtergrond = true
 function naamInput() {
     capyNaam = nameInput.value;
     h1.textContent = "Yaaay, " + capyNaam;
-    h2.textContent = "Ik ben " + capyNaam;
+    h2.textContent = "Ik ben magische " + capyNaam;
     nameInput.style.display = "none";
     button.style.display = "none";
     dobbel.style.display = "none";
